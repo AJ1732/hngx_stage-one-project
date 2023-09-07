ed6bc3e678e0f64cc28cd0db05f27bcd74ba169b
AJ1732 <ejemeniboi@gmail.com>
2023-09-07 21:44:01 +0100

    Dynamically Updated UTC time

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 const currentDate = new Date();
 
 // To get the current day of the week
-function getCurrentDayOfWeek() {
+const getCurrentDayOfWeek = () => {
   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
   return dayOfWeek;
 }
 
-// To get the current UTC time
-const getCurrentUTCTimeHMS = () => {
-  const hours = currentDate.getUTCHours().toString().padStart(2, "0");
-  const minutes = currentDate.getUTCMinutes().toString().padStart(2, "0");
-  const seconds = currentDate.getUTCSeconds().toString().padStart(2, "0");
-  return `${hours}:${minutes}:${seconds}`;
-}
-
+// 
 const getCurrentUTCMillisecond = () => {
-  // const milliseconds = currentDate.getUTCMilliseconds().toString();
   const currentUTCTimeMilliseconds = new Date().getTime();
   return `${currentUTCTimeMilliseconds}`
 }
 
-// Updating the Current Day of the Week and Current UTC Time elements in the HTML page
-document.addEventListener("DOMContentLoaded", () => {
+const updateUTCTime = () => {
   const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-  const currentUTCTimeElementHMS = document.querySelector('[data-testid="currentUTCTimeHMS"]');
   const currentUTCMillisecondElement = document.querySelector('[data-testid="currentUTCTime"]');
-
+  
   currentDayElement.textContent = `Current Day of the Week: ${getCurrentDayOfWeek()}`;
-  currentUTCTimeElementHMS.textContent = `${getCurrentUTCTimeHMS()}`;
   currentUTCMillisecondElement.textContent = `Current UTC Time: ${getCurrentUTCMillisecond()}`;
-});
+  
+  // const currentUTCTimeElementHMS = document.querySelector('[data-testid="currentUTCHMS"]');
+  // currentUTCTimeElementHMS.textContent = `${getCurrentUTCTimeHMS()}`;
+}
+
+// Updating the Current Day of the Week and Current UTC Time elements in the HTML page every 1 second
+setInterval(updateUTCTime, 1000);
+document.addEventListener("DOMContentLoaded", updateUTCTime);
+
+
+
+
+
+
+
+
+
+
+
+
+// To get the current UTC time
+// const getCurrentUTCTimeHMS = () => {
+//   const hours = currentDate.getUTCHours().toString().padStart(2, "0");
+//   const minutes = currentDate.getUTCMinutes().toString().padStart(2, "0");
+//   const seconds = currentDate.getUTCSeconds().toString().padStart(2, "0");
+//   return `${hours}:${minutes}:${seconds}`;
+// }
+
 
